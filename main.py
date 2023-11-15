1aa60c4222d255e0e71a44ae3bb1ff3db70e7685
Erik Vasilyan <110052720+erikvasilyan@users.noreply.github.com>
2023-11-15 16:10:32 +0100

    Actualització main.ts, main.py

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ def show_paper():
 def show_rock():
     basic.show_icon(IconNames.SMALL_SQUARE)
 
-def show_scissors:
+def show_scissors():
     basic.show_icon(IconNames.SCISSORS)
 
 def show_random_object():
-    objects = [show_rock, show_paper, show_scissors]
-    objects[0]()
+    rNumber = randint(1, 3)
+    if rNumber == 1:
+        show_paper()
+    if rNumber == 2:
+        show_rock()
+    if rNumber == 3:
+        show_scissors()
 
 input.on_button_pressed(Button.A, show_rock)
 input.on_button_pressed(Button.B, show_paper)
 input.on_button_pressed(Button.AB, show_scissors)
-
 input.on_gesture(Gesture.SHAKE, show_random_object)
--- main.ts
+++ main.ts
@@ -1,30 +1,30 @@
-input.onButtonPressed(Button.A, function show_rock() {
-    basic.showLeds(`
-    # # # # #
-    # . . . #
-    # . . . #
-    # . . . #
-    # # # # #
-    `)
-})
-input.onButtonPressed(Button.B, function show_paper() {
-    basic.showLeds(`
-    . . . . .
-    . # # # .
-    . # # # .
-    . # # # .
-    . . . . .
-    `)
-})
-input.onButtonPressed(Button.AB, function show_scissors() {
-    basic.showLeds(`
-    # # . . #
-    # # . # .
-    . . # . .
-    # # . # .
-    # # . . #
-    `)
-})
-input.onGesture(Gesture.Shake, function show_random_output() {
+function show_paper() {
+    basic.showIcon(IconNames.Square)
+}
+
+function show_rock() {
+    basic.showIcon(IconNames.SmallSquare)
+}
+
+function show_scissors() {
+    basic.showIcon(IconNames.Scissors)
+}
+
+input.onButtonPressed(Button.A, show_rock)
+input.onButtonPressed(Button.B, show_paper)
+input.onButtonPressed(Button.AB, show_scissors)
+input.onGesture(Gesture.Shake, function show_random_object() {
+    let rNumber = randint(1, 3)
+    if (rNumber == 1) {
+        show_paper()
+    }
+    
+    if (rNumber == 2) {
+        show_rock()
+    }
+    
+    if (rNumber == 3) {
+        show_scissors()
+    }
     
 })
